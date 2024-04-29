7b423d6d41b344cac2c1d9cc9972909e7160f3ff
ArgaAAL <argalumunon9@gmail.com>
2024-04-29 07:38:04 +0700

    menambahkan API mahasiswa

---
+++ modul10_1302223038/modul10_1302223038/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace modul10_1302223038
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+        public List<string> Course { get; set; }
+        public int Year { get; set; }
+
+        public Mahasiswa(string Nama, string Nim, List<string> Course, int Year) {
+            this.Nama = Nama;
+            this.Nim = Nim;
+            this.Course = Course;
+            this.Year = Year;
+        
+        }
+    }
+}
